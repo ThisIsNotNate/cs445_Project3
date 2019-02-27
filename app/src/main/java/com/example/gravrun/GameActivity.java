1e9a8c8336450227e952a2857cc918d92c1483fa
Nate Hilscher <nhilsch1@binghamton.edu>
2019-02-26 23:01:17 -0500

    Improved run animation speed

--- app/src/main/java/com/example/gravrun/GameActivity.java
+++ app/src/main/java/com/example/gravrun/GameActivity.java
@@ -2,12 +2,17 @@ package com.example.gravrun;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class GameActivity extends Activity {
 
@@ -16,8 +21,50 @@ public class GameActivity extends Activity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(new GameView(this));
 
+        GameView game = new GameView(this);
+        FrameLayout frame = new FrameLayout(this);
+        LinearLayout buttons = new LinearLayout(this);
+        Button invertGravity = new Button(this);
+        Button jump = new Button(this);
 
+        int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+        int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+        invertGravity.setX(0);
+        invertGravity.setY(0);
+        invertGravity.setWidth(screenWidth/2);
+        invertGravity.setHeight(screenHeight);
+        invertGravity.setVisibility(View.VISIBLE);
+        invertGravity.setBackgroundColor(Color.TRANSPARENT);
+
+        jump.setWidth(screenWidth/2);
+        jump.setHeight(screenHeight);
+        jump.setVisibility(View.VISIBLE);
+        jump.setBackgroundColor(Color.TRANSPARENT);
+
+        buttons.addView(invertGravity);
+        buttons.addView(jump);
+
+        frame.addView(game);
+        frame.addView(buttons);
+
+        setContentView(frame);
+
+        invertGravity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Button Pressed", "Invert Gravity");
+                //game.invertGravity()
+            }
+        });
+
+        jump.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Button Pressed", "Jump");
+                //game.jump()
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/gravrun/GameView.java
+++ app/src/main/java/com/example/gravrun/GameView.java
@@ -1,6 +1,9 @@
 package com.example.gravrun;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -8,38 +11,54 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
+import android.view.View;
+import android.widget.Button;
 
 public class GameView extends SurfaceView implements Runnable {
-    private Thread gameThread = null;
+    int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+
+    private Thread gameThread = null; // I used this tutorial for animating spritesheets, but I custom made the spritesheet. http://gamecodeschool.com/android/coding-android-sprite-sheet-animations/
     long fps=1;
     long timeThisFrame;
     long lastFrameChangeTime = 0;
 
-    float gravRunnerX = 200;
     private int frameWidth = 75;
     private int frameHeight = 108;
+
+    float gravRunnerX = 200;
+    float gravRunnerY = screenHeight-50-frameHeight;
+
     Bitmap gravRunner;
     private int frameCount = 8;
     private int currentFrame = 0;
     private int frameDuration = 50; // in ms
     private Rect spriteFrame = new Rect(0,0,frameWidth, frameHeight);
-    private RectF drawFrame = new RectF(gravRunnerX,0, gravRunnerX+frameWidth, frameHeight);
+    private RectF drawFrame = new RectF(gravRunnerX,gravRunnerY, gravRunnerX+frameWidth, gravRunnerY+frameHeight);
 
     private SurfaceHolder surfaceHolder;
     private boolean running;
     public static Canvas canvas;
     public Paint paint;
 
+    Bitmap floor;
+
+    private Rect floorFrame = new Rect(0, 0, screenHeight,50);
+
 
 
     public GameView(Context context){
         super(context);
+
         surfaceHolder = getHolder();
         gravRunner = BitmapFactory.decodeResource(this.getResources(), R.drawable.gravrunner_spritesheet);
         gravRunner = Bitmap.createScaledBitmap(gravRunner, frameWidth*frameCount, frameHeight, false);
+        floor = BitmapFactory.decodeResource(this.getResources(), R.mipmap.game_floor);
+        floor = Bitmap.createScaledBitmap(floor, screenHeight, 50, false);
 
         paint = new Paint();
 
@@ -52,15 +71,17 @@ public class GameView extends SurfaceView implements Runnable {
 
     public void update(){
         //where we'll update game code
-        //gravRunnerX = gravRunnerX + (25/fps);
+        //gravRunnerX = gravRunnerX + (60/fps);
     }
 
-    @Override
+    @SuppressLint("MissingSuperCall")
     public void draw(Canvas canvas){
 
 
         if(surfaceHolder.getSurface().isValid()){
             canvas = surfaceHolder.lockCanvas();
+
+            drawFrame.set(gravRunnerX,gravRunnerY, gravRunnerX+frameWidth, gravRunnerY+frameHeight);
 
             canvas.drawColor(Color.argb(255, 26, 128, 182));
             paint.setColor(Color.argb(255, 249, 129, 0));
@@ -68,10 +89,12 @@ public class GameView extends SurfaceView implements Runnable {
 
             canvas.drawText("FPS:"+fps, 20, 40, paint);
 
-            drawFrame.set((int) gravRunnerX, 0, (int)gravRunnerX+frameWidth, frameHeight);
+            drawFrame.set(gravRunnerX, gravRunnerY, gravRunnerX+frameWidth, gravRunnerY+frameHeight);
             getCurrentFrame();
             canvas.drawBitmap(gravRunner, spriteFrame, drawFrame, paint);
-
+            paint.setColor(Color.argb(255, 90, 90, 90));
+            canvas.drawRect(0,0,screenWidth,50,paint);
+            canvas.drawRect(0,screenHeight-50,screenWidth,screenHeight,paint);
             surfaceHolder.unlockCanvasAndPost(canvas);
         }
         //super.draw(canvas);
